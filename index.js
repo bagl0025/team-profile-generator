8c3660a80bb5b1ff5d3f7466a088865bedf6f6bb
bagl0025 <bagl0025@umn.edu>
2022-05-10 12:04:28 -0500

    completed the question portion and adding all team members to array

--- index.js
+++ index.js
@@ -4,120 +4,171 @@ const Intern = require('./lib/Intern');
 const Engineer = require('./lib/Engineer');
 const Manager = require('./lib/Manager');
 
+// create employee array
+var employees = [];
+
 function runApp () {
-    // save employees as object
-    var employees = [];
-    // complete set of questions
+    // complete set of questions. I will create subsets later.
+    // this has basic validation to prevent skipping question, but
+    // it won't verify email etc.
     const questions = [
-            {
-                type: 'text',
-                name: 'name',
-                message: "Enter the employee's name.",
-                validate: nameInput => {
-                    if (nameInput) {
-                        return true;
-                    } else {
-                        console.log ("Enter employee's name.");
-                        return false; 
-                    }
+        {
+            type: 'text',
+            name: 'name',
+            message: "Enter the employee's name.",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log ("Enter employee's name.");
+                    return false; 
                 }
-            },
-            {
-                type: 'text',
-                name: 'id',
-                message: "Enter the employee's id.",
-                validate: idInput => {
-                    if (idInput) {
-                        return true;
-                    } else {
-                        console.log ("Enter an id.");
-                        return false; 
-                    }
+            }
+        },
+        {
+            type: 'text',
+            name: 'id',
+            message: "Enter the employee's id.",
+            validate: idInput => {
+                if (idInput) {
+                    return true;
+                } else {
+                    console.log ("Enter an id.");
+                    return false; 
                 }
-            },
-            {
-                type: 'text',
-                name: 'email',
-                message: "Enter the employee's email.",
-                validate: emailInput => {
-                    if (emailInput) {
-                        return true;
-                    } else {
-                        console.log ("Enter an email adress.");
-                        return false; 
-                    }
+            }
+        },
+        {
+            type: 'text',
+            name: 'email',
+            message: "Enter the employee's email.",
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log ("Enter an email adress.");
+                    return false; 
                 }
-            },
-            {
-                type: 'text',
-                name: 'office',
-                message: "Enter employee's office number.",
-                validate: officeInput => {
-                    if (officeInput) {
-                        return true;
-                    } else {
-                        console.log ("Enter an office number.");
-                        return false; 
-                    }
+            }
+        },
+        {
+            type: 'text',
+            name: 'officeNumber',
+            message: "Enter employee's office number.",
+            validate: officeInput => {
+                if (officeInput) {
+                    return true;
+                } else {
+                    console.log ("Enter an office number.");
+                    return false; 
                 }
-            },
+            }
+        },
+        {
+            type: 'text',
+            name: 'github',
+            message: "Enter the engineer's GitHub user name.",
+            validate: gitInput => {
+                if (gitInput) {
+                    return true;
+                } 
+                else {
+                    console.log ("Enter a GitHub user name.");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'text',
+            name: 'school',
+            message: "Enter the intern's school name.",
+            validate: schoolInput => {
+                if (schoolInput) {
+                    return true;
+                } 
+                else {
+                    console.log ("Enter an school name.");
+                    return false;
+                }
+            }
+        }
+    ] // end of question block
+
+    // create subsets of questions for each role
+    const managerQuestions = [...questions];
+    managerQuestions.splice(4,2);
+    const internQuestions = [...questions];
+    internQuestions.splice(3,4,questions[5]);
+    const engineerQuestions = [...questions];
+    engineerQuestions.splice(3,4,questions[4]);
+
+    // if the array is empty it's the first run, so it's manager
+    if (employees.length === 0) {
+        addManager();
+    }
+    else {
+        pickRole();
+    }
+
+    function pickRole () {
+        return inquirer.prompt ([
             {
                 type: 'list',
                 name: 'employeeRole',
                 message: "Select an employee role from the list.",
                 choices: ['Engineer', 'Intern', 'Finish Roster']  
-            },
-            {
-                type: 'text',
-                name: 'github',
-                message: "Enter the engineer's GitHub user name.",
-                validate: gitInput => {
-                    if (gitInput) {
-                        return true;
-                    } 
-                    else {
-                        console.log ("Enter a GitHub user name.");
-                        return false;
-                    }
-                }
-            },
-            {
-                type: 'text',
-                name: 'school',
-                message: "Enter the intern's school name.",
-                validate: schoolInput => {
-                    if (schoolInput) {
-                        return true;
-                    } 
-                    else {
-                        console.log ("Enter an school name.");
-                        return false;
-                    }
-                }
             }
-        ]
-        // create subsets of questions for each role
-        const managerQuestions = [...questions];
-        managerQuestions.splice(5,2);
-        const internQuestions = [...questions];
-        internQuestions.splice(3,4,questions[6],questions[4]);
-        const engineerQuestions = [...questions];
-        engineerQuestions.splice(3,4,questions[5],questions[4]);
+        ])
+        .then(answers => {
+            if (answers.employeeRole === "Engineer") {
+                addEngineer();
+            } 
+            else if (answers.employeeRole === "Intern") {
+                addIntern();
+            }
+            else {
+                console.log(employees);
+                return false;
+            }
+        });    
+    }
 
-        console.log(managerQuestions);
-        console.log(managerQuestions.length);
-        console.log(internQuestions);
-        console.log(internQuestions.length);
-        console.log(engineerQuestions);
-        console.log(engineerQuestions.length);
+    // these 3 functions are very similar. If I have time, try to combine into 1.
+    function addManager () {
+        return inquirer.prompt (managerQuestions)
+        .then(answers => {
+            // objectify
+            const  { name, id, email, officeNumber} = answers; 
+            const manager = new Manager (name, id, email, officeNumber);
+            employees.push(manager);
+            pickRole();
+        }); 
+    }
 
-        if (employees.length === 0) {
-            inquirer.prompt (managerQuestions);
-        }
-        // write to array
-        // .then check role call next set of prompts
-        
+    function addEngineer () {
+        return inquirer.prompt (engineerQuestions)
+        .then(answers => {
+            // objectify
+            const  { name, id, email, github} = answers; 
+            const engineer = new Engineer (name, id, email, github);
+            employees.push(engineer);
+            pickRole();
+        }); 
+    }
+
+    function addIntern () {
+    return inquirer.prompt (internQuestions)
+        .then(answers => {
+        // objectify
+        const  { name, id, email, school} = answers; 
+        const intern = new Intern (name, id, email, school);
+        employees.push(intern);
+        pickRole();
+        });
+    }
+           
         // need genereate html
         // css styling
+
 } // end runApp
 runApp();
